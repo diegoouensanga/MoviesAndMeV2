ba06dbad392819b46bff27646e8a4b9b1b0e2c1b
diegoouensanga <diego.ouensanga@gmail.com>
2019-06-05 07:25:22 +0200

    Creation

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Text from 'react-native'
+
+export default class App extends React.Component {
+  render() {
+    return (
+        <Text>"Hello"</Text>
+
+    )
+  }
+}
+
+
